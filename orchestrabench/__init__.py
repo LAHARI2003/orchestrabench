599b796edefc030204e719b3a83d4464e87b5c35
LAHARI2003 <lahari1803ks@gmail.com>
2026-08-13 02:02:39 +0530

    chore: create OrchestraBench base project

---
+++ orchestrabench/__init__.py
@@ -0,0 +1,1 @@
+"""OrchestraBench package."""
---
+++ orchestrabench/runner.py
@@ -0,0 +1,9 @@
+"""Placeholder runner for OrchestraBench."""
+
+
+def run():
+    """Run a benchmark suite.
+
+    Benchmark execution will be implemented in a future iteration.
+    """
+    raise NotImplementedError
